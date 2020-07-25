a55ac555ceae431a4d88d1547be60440c33a14e5
turui <550228957@qq.com>
2020-07-25 21:15:10 +0800

    add resource server.

---
+++ auth-server/src/main/java/com/trey/authserver/entity/BsUser.java
@@ -0,0 +1,44 @@
+package com.trey.authserver.entity;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Data
+public class BsUser implements UserDetails {
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private Date createDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
